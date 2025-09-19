e64109826eb07a43e72e3ce9481c9aec3eef59ac
Abigayle Callahan <abicalla1@WKMZT08556CD.global.publicisgroupe.net>
2025-09-19 14:10:34 -0400

    whole websiote first draft

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif'
+};
+
+const server = http.createServer((req, res) => {
+  const filePath = req.url === '/' ? './home.html' : `.${req.url}`;
+  const extname = path.extname(filePath);
+  const contentType = mimeTypes[extname] || 'text/plain';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
